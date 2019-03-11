1e9d10d08b3b6bc53e298082f803068fa3db36cd
unknown <duytrankc1@gmail.com>
2019-03-11 19:11:51 +0700

    update cac ham tinh toan

--- app/src/main/java/com/example/calculator/Presenter/CalculatorPresenter.java
+++ app/src/main/java/com/example/calculator/Presenter/CalculatorPresenter.java
@@ -3,6 +3,8 @@ package com.example.calculator.Presenter;
 import com.example.calculator.Algorithm.PostFix;
 import com.example.calculator.MathImpl.Operation;
 
+import java.util.Stack;
+
 /**
  * @author Dattr
  * @version 1.0
@@ -22,9 +24,42 @@ public class CalculatorPresenter {
 	public CalculatorPresenter(String x){
 
 	}
+	//D
+	public double compute(Stack<String> st){
+	    Stack<String> stack = new Stack<>();
+	    String tmp = "";
+	    while (!st.empty())
+        {
+            tmp = st.pop();
+            if(isOperator(tmp)==0) stack.push(tmp);
+            else {
+                double number1 = Double.parseDouble(stack.pop());
+                double number2 = Double.parseDouble(stack.pop());
+                stack.push(caculartor(number1,number2,tmp)+"");
+            }
+        }
+		return Double.parseDouble(stack.pop());
+	}
 
-	public double compute(String x){
-		return 0;
+	private int isOperator(String s)
+    {
+        if(s.equals("+")) return 1;
+        else if(s.equals("-")) return 1;
+        else if(s.equals("*")) return 1;
+        else if(s.equals("/")) return 1;
+        else return  0;
+    }
+
+	private double caculartor(double a, double b, String operator)
+	{
+	    Operation operation = new Operation();
+	    switch (operator){
+            case "+":return operation.addExpr(a,b);
+            case  "-" : return operation.subExpr(a,b);
+            case  "*": return  operation.multiExpr(a,b);
+            case "/" : return  operation.divExpr(a,b);
+            default: throw new RuntimeException("Lỗi không xác định");
+        }
 	}
 
 	private void detectExpression(){
